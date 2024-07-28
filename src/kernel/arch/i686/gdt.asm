dee719958f97e966845e5edef88e8bd6323b49e3
Macdotexe <parsaarya2012@gmail.com>
2024-07-28 14:00:36 +0300

    Implemented IDT and bug fixes

--- src/kernel/arch/i686/gdt.asm
+++ src/kernel/arch/i686/gdt.asm
@@ -1,4 +1,4 @@
-bits 32
+[bits 32]
 
 ;
 ; void __attribute__((cdecl)) i686_GDT_Load(
@@ -9,29 +9,30 @@ bits 32
 ;
 global i686_GDT_Load
 i686_GDT_Load:
+    
     ; make new call frame
-    push ebp            ; save old call frame
-    mov ebp, esp        ; init new call frame
-
+    push ebp             ; save old call frame
+    mov ebp, esp         ; initialize new call frame
+    
     ; load gdt
     mov eax, [ebp + 8]
     lgdt [eax]
 
     ; reload code segment
     mov eax, [ebp + 12]
+    push eax
     push .reload_cs
     retf
 
 .reload_cs:
 
-    ; reload data segment
-    mov eax, [ebp + 16]
+    ; reload data segments
+    mov ax, [ebp + 16]
     mov ds, ax
     mov es, ax
     mov fs, ax
-    mov gs, ax
+    mov gs, ax,
     mov ss, ax
-
 
     ; restore old call frame
     mov esp, ebp
--- src/kernel/arch/i686/gdt.c
+++ src/kernel/arch/i686/gdt.c
@@ -22,24 +22,25 @@ typedef enum
     GDT_ACCESS_CODE_READABLE                = 0x02,
     GDT_ACCESS_DATA_WRITEABLE               = 0x02,
 
-    GDT_ADDRESS_CODE_CONFORMING             = 0x04,
+    GDT_ACCESS_CODE_CONFORMING              = 0x04,
     GDT_ACCESS_DATA_DIRECTION_NORMAL        = 0x00,
-    GDT_ACCESS_DATA_DIRECTION_DOWN          = 0x00,
+    GDT_ACCESS_DATA_DIRECTION_DOWN          = 0x04,
 
-    GDT_ACCESS_DATA_SEGMENT                 = 0x00,
-    GDT_ACCESS_CODE_SEGMENT                 = 0x00,
-    
-    GDT_ACCESS_DESCRIPTOR                   = 0x00,
+    GDT_ACCESS_DATA_SEGMENT                 = 0x10,
+    GDT_ACCESS_CODE_SEGMENT                 = 0x18,
+
+    GDT_ACCESS_DESCRIPTOR_TSS               = 0x00,
 
     GDT_ACCESS_RING0                        = 0x00,
-    GDT_ACCESS_RING1                        = 0x00,
-    GDT_ACCESS_RING2                        = 0x00,
-    GDT_ACCESS_RING3                        = 0x00,
+    GDT_ACCESS_RING1                        = 0x20,
+    GDT_ACCESS_RING2                        = 0x40,
+    GDT_ACCESS_RING3                        = 0x60,
 
-    GDT_ACCESS_PRESENT                      = 0x80
+    GDT_ACCESS_PRESENT                      = 0x80,
+
 } GDT_ACCESS;
 
-typedef enum
+typedef enum 
 {
     GDT_FLAG_64BIT                          = 0x20,
     GDT_FLAG_32BIT                          = 0x40,
@@ -99,7 +100,7 @@ GDTEntry g_GDT[] = {
 GDTDescriptor g_GDTDescriptor = 
 {
     sizeof(g_GDT) - 1,
-    &g_GDT
+    g_GDT
 };
 
 void __attribute__((cdecl)) i686_GDT_Load(
