fc98d26e1403688e49d4e06dc66b3920cd242ae7
Macdotexe <parsaarya2012@gmail.com>
2024-09-03 07:59:07 +0300

    New test program

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,6 +4,7 @@
 #include <hal/hal.h>
 #include "memory.h"
 #include "drivers/kbd.h"
+#include "arch/i686/io.h"
 
 extern uint8_t __bss_start;
 extern uint8_t __end;
@@ -18,21 +19,9 @@ void __attribute__((section(".entry"))) start(uint16_t bootDrive)
 
     printf("MinimOS Kernel Loaded\n");
 
-    // Test program
-    printf("=== Test Program ===\n");
-    
-
-    // uint32_t* num1;
-    // uint32_t* num2;
-
-    // printf("Enter number 1: ");
-    // KBD_ReadNumber(num1);
-    // printf("Enter number 2: ");
-    // KBD_ReadNumber(num2);
-
-    // printf("Result: %d\n", *num1 + *num2);
-
 end:
-    printf("End of program: halted\n");
-    for (;;);
+    for (;;)
+    {
+        KBD_ReadLine(0);
+    }
 }
